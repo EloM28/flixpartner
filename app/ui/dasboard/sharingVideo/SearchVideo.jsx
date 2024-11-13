b7e0ef63ee5da170e8e3071343388571f3ff992c
EloM28 <mugisheloge@gmail.com>
2024-11-13 10:57:35 +0200

    Added search front-end

--- app/ui/dasboard/sharingVideo/SearchVideo.jsx
+++ app/ui/dasboard/sharingVideo/SearchVideo.jsx
@@ -42,7 +42,21 @@ const SearchVideo = () => {
 
   return (
     <>
-      <div id="load_data" className={`dark:bg-medium dark:border-medium dark:border-t-white border-4 border-t-blue-500  border-white pt-[30px] grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-16  gap-3 `}>
+    <div className='mt-16'>
+      <form class="max-w-md mx-auto">   
+        <label for="" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Video......" required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+      </form>
+    </div>
+      <div id="load_data" className={`dark:bg-medium border-white pt-[30px] grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-3 `}>
         {error ? (
           <div>{error}</div>
         ) : videos != null ? (
